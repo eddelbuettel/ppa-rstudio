17a6649fee1044b2057d2dbc046e8ca219e5ffca
Dirk Eddelbuettel <edd@debian.org>
2025-04-09 07:53:22 -0500

    Simpler approach to extract leading package name

--- pruneDebs.r
+++ pruneDebs.r
@@ -19,13 +19,14 @@ if (!dir.exists(srcdir)) q("no")
 setwd(srcdir)
 files <- list.files(pattern="*.deb", path=".")
 df <- data.table(files=files, file.info(files)[,c(1,5)])
-df[, `:=`(package=gsub("(.*-?)-([0-9]*\\.[0-9]*\\.[0-9]*-.*)(-amd64)?\\.deb", "\\1", files),
-          version=gsub("(.*-?)-([0-9]*\\.[0-9]*\\.[0-9]*((-daily)?-\\d*)?-.*)(-amd64)?\\.deb", "\\2", files))]
-df[, version := gsub("-daily", "", version)]
-setkey(df, package, ctime, version)
+#df[, `:=`(package=gsub("(.*-?)-([0-9]*\\.[0-9]*\\.[0-9]*-.*)(-amd64|-x64)?\\.deb", "\\1", files),
+#          version=gsub("(.*-?)-([0-9]*\\.[0-9]*\\.[0-9]*((-daily)?-\\d*)?-.*)(-amd64|-x64)?\\.deb", "\\2", files))]
+#df[, version := gsub("-daily", "", version)]
+df[, `:=`(package=gsub("^([a-zA-Z-]*-?)(-|_)[0-9].*\\.deb", "\\1", files))]
+setkey(df, package, ctime)
 if (opt$verbose) print(df)
 
-sdf <- df[, .(files, version, .N, ctime), by=package][N>1, ]
+sdf <- df[, .(files, .N, ctime), by=package][N>1, ]
 if (opt$verbose) print(sdf)
 sdf[, keep := as.logical(files==tail(files,1)), by=package]
 if (opt$verbose) print(sdf)

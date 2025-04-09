7a8bb77633ce8a336e707725dd13152f8915e0d8
Dirk Eddelbuettel <edd@debian.org>
2025-04-09 07:53:00 -0500

    Positron deb needs to get pulled out of body

--- getPositron.r
+++ getPositron.r
@@ -8,9 +8,13 @@
 
 url <- "https://api.github.com/repos/posit-dev/positron/releases"
 res <- jsonlite::fromJSON(url)
-files <- res$assets[[1]][, "browser_download_url"]
+#files <- res$assets[[1]][, "browser_download_url"]
 #deb <- files[grepl("amd64\\.deb$", files)]
-deb <- files[grepl("\\.deb$", files)]
+#deb <- files[grepl("\\.deb$", files)]
+
+## hidden in body text
+deb <- gsub(".*(https://cdn.posit.co/positron/dailies/deb/x86_64/Positron-.*x64.deb).*", "\\1", res$body[1])
+
 tgt <- basename(deb)
 if (!file.exists(tgt)) download.file(deb, tgt, quiet=TRUE)
 #cat("Downloaded '", deb, "' as '", tgt, "'\n", sep="")
